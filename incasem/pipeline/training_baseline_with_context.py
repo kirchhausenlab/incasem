1b98f2e787353c22604d1948f8328d4b4670a85d
Athul Nair <42418601+athulnair02@users.noreply.github.com>
2023-08-02 05:36:13 -0400

    Support for regions smaller than (204, 204, 204) (#14)
    Fix by adding padding of BACKGROUND_MASK.

--- incasem/pipeline/training_baseline_with_context.py
+++ incasem/pipeline/training_baseline_with_context.py
@@ -28,8 +28,8 @@ class TrainingBaselineWithContext:
             voxel_size,
             input_size_voxels,
             output_size_voxels,
-            reject_min_masked=0.0,
-            reject_probability=1.0,
+            reject_min_masked=0.05,
+            reject_probability=0.9,
             random_seed=None,
     ):
         self._data_config = data_config
@@ -130,6 +130,11 @@ class TrainingBaselineWithContext:
                     keys['MASK'],
                     size=None,
                     value=1
+                )
+                + fos.gunpowder.PadDownstreamOfRandomLocation(
+                    keys['BACKGROUND_MASK'],
+                    size=None,
+                    value=0
                 )
                 + fos.gunpowder.PadDownstreamOfRandomLocation(
                     keys['METRIC_MASK'],
