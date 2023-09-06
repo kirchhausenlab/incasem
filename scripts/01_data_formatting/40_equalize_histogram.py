91ac8150000b3bbf87ac71b5a8e44b6315801445
Patrick Stock <pstock175@gmail.com>
2023-09-06 11:31:45 -0400

    Update 40_equalize_histogram.py

--- scripts/01_data_formatting/40_equalize_histogram.py
+++ scripts/01_data_formatting/40_equalize_histogram.py
@@ -85,7 +85,9 @@ def equalize_histogram(
     print(">>>", raw.voxel_size)
     print(raw.voxel_size[0])
     print(type(raw.voxel_size))
-    rint(type(raw.voxel_size[0]))
+    print(type(raw.voxel_size[0]))
+
+    print((1,) * raw.voxel_size.dims)
                 
     context = ((raw.voxel_size * kernel_size) + (1,) * raw.voxel_size.dims) / 2
     read_roi = write_roi.grow(context, context)
