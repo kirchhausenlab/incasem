a16ce79bef0657a16a3f1dd18009684967a13cce
ArkashJ <arkjain@bu.edu>
2024-10-21 16:10:14 -0400

    incasem updates

--- download_models.py
+++ download_models.py
@@ -6,4 +6,4 @@ b = quilt3.Bucket("s3://asem-project")
 b.fetch("models/", "~/incasem/models/")
 
 # load database dump into local mongodb
-#os.popen('mongorestore --archive="models/fiborganelle_trainings" --nsFrom="public_fiborganelle.*" --nsTo="incasem_trainings.*"').read()
+# os.popen('mongorestore --archive="models/fiborganelle_trainings" --nsFrom="public_fiborganelle.*" --nsTo="incasem_trainings.*"').read()
