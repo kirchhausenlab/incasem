9131adf5c589ff766051b6fa8717058fe1b72ef7
ArkashJ <arkjain@bu.edu>
2024-10-24 17:14:39 -0400

    updated

--- download_models.py
+++ download_models.py
@@ -4,6 +4,6 @@ import quilt3
 # download from AWS bucket
 b = quilt3.Bucket("s3://asem-project")
 
-b.fetch("datasets/cell_1/cell_1.zarr/volumes/labels", "cell_6/cell_6.zarr/")
+b.fetch("datasets/cell_1/cell_1.zarr/", "cell_6/cell_6.zarr/")
 # load database dump into local mongodb
 # os.popen('mongorestore --archive="models/fiborganelle_trainings" --nsFrom="public_fiborganelle.*" --nsTo="incasem_trainings.*"').read()
